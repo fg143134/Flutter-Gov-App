ded53d876a0505be5a2b36e72d9e04f44fdf53af
bakr <abubaker.fg@gmail.com>
2022-10-25 17:33:39 +0200

    abu

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:es_eidc_app/productmodule/views/eservices_list_view.dart';
 import 'package:es_eidc_app/commonmodule/AppColor.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
